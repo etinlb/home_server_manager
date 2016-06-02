cd7def39b0a483fa5d7f704b2e80a1f81300c7d2
Erik Parreira <etinlb@gmail.com>
2016-06-02 10:12:44 -0700

    Testing angular on the frontend
    Not to sure I like it but we’ll see

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,47 @@
-console.log("hello");
+var server_manager = angular.module('server_manager', []).controller("mainController");
+
+
+function mainController($scope, $http) {
+    // when landing on the page, get all torrents and show them? Naw....
+    // TODO: SHould be a get with the url as parameter
+    $http({
+            url: '/api/cmd',
+            method: "POST",
+            data: JSON.stringify( {"action" : "list_dir", "args":{ "dir" : "/Users/erikparreira/Dropbox/Programming/home_server_manager/test_data"}}),
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            console.log("DONE!");
+            console.log(data);
+        }).error(function (data, status, headers, config) {
+            $scope.status = status + ' ' + headers;
+        });
+
+     $scope.fix_permissions = function() {
+        $http({
+            url: '/api/cmd',
+            method: "POST",
+            data: JSON.stringify({args:{ "dir" : "fix_all_permissions"}}),
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            console.log("DONE!")
+        }).error(function (data, status, headers, config) {
+            $scope.status = status + ' ' + headers;
+        });
+    }
+
+    $scope.get_dir = function() {
+        $http({
+            url: '/api/cmd',
+            method: "POST",
+            data: JSON.stringify({action:"fix_all_permissions"}),
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            console.log("DONE!")
+        }).error(function (data, status, headers, config) {
+            $scope.status = status + ' ' + headers;
+        });
+    }
+
+}
+
+server_manager.controller("mainController", mainController);
